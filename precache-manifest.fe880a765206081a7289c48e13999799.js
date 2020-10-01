91b183a014916e2ecaa17d5f8474d757aa3d214e
xakim <xakim@pop-os.localdomain>
2020-10-01 20:41:53 +0600

    Updates

---
+++ precache-manifest.fe880a765206081a7289c48e13999799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8213b5de7a2dd6e1cc0a2af80b71ce",
+    "url": "/ElectionsMonitoringFeed/index.html"
+  },
+  {
+    "revision": "3218fbde7289b4cdc133",
+    "url": "/ElectionsMonitoringFeed/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "57ec437ba4c183bc9f61",
+    "url": "/ElectionsMonitoringFeed/static/js/2.1fbccb87.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ElectionsMonitoringFeed/static/js/2.1fbccb87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3218fbde7289b4cdc133",
+    "url": "/ElectionsMonitoringFeed/static/js/main.c7ac3aa8.chunk.js"
+  },
+  {
+    "revision": "ae4deda12519688e58fc",
+    "url": "/ElectionsMonitoringFeed/static/js/runtime-main.6d7a2b36.js"
+  }
+]);
