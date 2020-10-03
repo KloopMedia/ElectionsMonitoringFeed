0b256faeb3c874780b566f7c5107fc73cb0882c2
xakim <xakim@pop-os.localdomain>
2020-10-03 20:00:36 +0600

    Updates

--- precache-manifest.b78888bb028ed9320dc5e391bbe84836.js
+++ precache-manifest.b78888bb028ed9320dc5e391bbe84836.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c0ccacc42173a3d9989a13e8ebedb98",
+    "revision": "3f7f1c31f32d568b549c7b0b681b21a8",
     "url": "/ElectionsMonitoringFeed/index.html"
   },
   {
-    "revision": "1bb24a8a8275862bc99c",
+    "revision": "681503d536a3bdce9ac5",
     "url": "/ElectionsMonitoringFeed/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ElectionsMonitoringFeed/static/js/2.fc9bdd1b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1bb24a8a8275862bc99c",
-    "url": "/ElectionsMonitoringFeed/static/js/main.f88b83a2.chunk.js"
+    "revision": "681503d536a3bdce9ac5",
+    "url": "/ElectionsMonitoringFeed/static/js/main.cdb53e64.chunk.js"
   },
   {
     "revision": "ae4deda12519688e58fc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ElectionsMonitoringFeed/precache-manifest.1bd2e5621dc20e90cc2f37ede05c6588.js"
+  "/ElectionsMonitoringFeed/precache-manifest.b78888bb028ed9320dc5e391bbe84836.js"
 );
 
 self.addEventListener('message', (event) => {
